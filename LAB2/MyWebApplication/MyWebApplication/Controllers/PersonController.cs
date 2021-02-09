8fa2b648e8bb2b93cbfdfdfc5c30ab9e89a7a851
NayeemHasan807 <moonnayeem87@gmail.com>
2021-02-10 01:47:51 +0600

    Data transfer from server to client

---
+++ LAB2/MyWebApplication/MyWebApplication/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyWebApplication.Models;
+
+namespace MyWebApplication.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            var person = new Person();
+            person.Name = "Alaxa";
+            person.Salary = "100000";
+
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person() { Name = "Denver", Salary = "20000" });
+            personList.Add(new Person() { Name = "Tokyo", Salary = "40000" });
+            return View(personList);
+
+        }
+    }
+}
