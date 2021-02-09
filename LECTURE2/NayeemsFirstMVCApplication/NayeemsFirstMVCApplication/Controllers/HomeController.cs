e859a15f6d80d83020db3bfee1c97d9003752a90
NayeemHasan807 <moonnayeem87@gmail.com>
2021-02-10 01:46:45 +0600

    Request And Response

---
+++ LECTURE2/NayeemsFirstMVCApplication/NayeemsFirstMVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NayeemsFirstMVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello .Net from Home";
+        }
+        public string MyMethod(int? id)
+        {
+            return "Yep It Works "+id;
+        }
+    }
+}
