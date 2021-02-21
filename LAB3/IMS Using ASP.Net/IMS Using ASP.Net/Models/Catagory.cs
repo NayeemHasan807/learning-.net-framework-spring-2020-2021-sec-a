78311b648cd6ad838cd4820bd550801bf53845a5
NayeemHasan807 <moonnayeem87@gmail.com>
2021-02-21 20:16:46 +0600

    Catagory class containing properties of catagories table

---
+++ LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Models/Catagory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_Management_System_With_ADO.Net.Models
+{
+    public class Catagory
+    {
+        public int CatagoryId { get; set; }
+        public string CatagoryName { get; set; }
+    }
+}
