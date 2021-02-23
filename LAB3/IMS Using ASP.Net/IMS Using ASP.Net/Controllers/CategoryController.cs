2ebaf348d3ae49f128fa91fc88a7f1af54bf540c
NayeemHasan807 <moonnayeem87@gmail.com>
2021-02-24 02:39:42 +0600

    CategoryModel, CategoryController & CategoryList view index

---
+++ LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Inventory_Management_System_With_ADO.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_Management_System_With_ADO.Net.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryModel categoryModel = new CategoryModel();
+        public ActionResult Index()
+        {
+            return View(categoryModel.GetCategories());
+        }
+    }
+}
---
+++ LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Models/CategoryModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_Management_System_With_ADO.Net.Models
+{
+    public class CategoryModel
+    {
+        DataAccess dataAccess;
+
+        public CategoryModel()
+        {
+            dataAccess = new DataAccess();
+        }
+
+        public List<Category> GetCategories()
+        {
+            string sql = "SELECT * FROM Categories";
+            SqlDataReader reader = dataAccess.GetData(sql);
+            List<Category> categories = new List<Category>();
+            while (reader.Read())
+            {
+                Category category = new Category();
+                category.CategoryId = (int)reader["CategoryId"];
+                category.CategoryName = reader["CategoryName"].ToString();
+                categories.Add(category);
+            }
+
+            return categories;
+        }
+
+    }
+}
