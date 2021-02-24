7e8a115243711728983c06f5b88c2416132b89fc
NayeemHasan807 <moonnayeem87@gmail.com>
2021-02-24 18:53:25 +0600

    Category CRUD

--- LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Controllers/CategoryController.cs
+++ LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Controllers/CategoryController.cs
@@ -10,9 +10,46 @@ namespace Inventory_Management_System_With_ADO.Net.Controllers
     public class CategoryController : Controller
     {
         CategoryModel categoryModel = new CategoryModel();
+        
+        [HttpGet]
         public ActionResult Index()
         {
             return View(categoryModel.GetCategories());
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryModel.Insert(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(categoryModel.GetCategory(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Category category)
+        {
+            category.CategoryId = id;
+            categoryModel.Update(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View(categoryModel.GetCategory(id));
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            categoryModel.Delete(id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Models/CategoryModel.cs
+++ LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Models/CategoryModel.cs
@@ -31,5 +31,37 @@ namespace Inventory_Management_System_With_ADO.Net.Models
             return categories;
         }
 
+        public Category GetCategory(int id)
+        {
+            string sql = "SELECT * FROM Categories Where categoryid="+id;
+            SqlDataReader reader = dataAccess.GetData(sql);
+            reader.Read();
+            Category category = new Category();
+            category.CategoryId = (int)reader["CategoryId"];
+            category.CategoryName = reader["CategoryName"].ToString();
+
+            return category;
+        }
+
+        public int Insert(Category category)
+        {
+            string sql = "INSERT INTO Categories(CategoryName) VALUES('" + category.CategoryName + "')";
+            int result = dataAccess.ExecuteQuery(sql);
+            return result;
+        }
+        
+        public int Update(Category category)
+        {
+            string sql = "UPDATE Categories SET CategoryName='" + category.CategoryName + "' WHERE CategoryId=" + category.CategoryId;
+            int result = dataAccess.ExecuteQuery(sql);
+            return result;
+        }
+        
+        public int Delete(int id)
+        {
+            string sql = "DELETE FROM Categories WHERE CategoryId=" + id;
+            int result = dataAccess.ExecuteQuery(sql);
+            return result;
+        }
     }
 }
