5a016995b8a00e87a33890f995e6423827cd2163
NayeemHasan807 <moonnayeem87@gmail.com>
2021-02-24 20:27:44 +0600

    ProductModel, ProductController & Index to view product list

---
+++ LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Inventory_Management_System_With_ADO.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_Management_System_With_ADO.Net.Controllers
+{
+    public class ProductController : Controller
+    {
+        CategoryModel categoryModel = new CategoryModel();
+        ProductModel productModel = new ProductModel();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewData["categories"] = categoryModel.GetCategories();
+            return View(productModel.GetProducts());
+        }
+    }
+}
---
+++ LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Models/ProductModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_Management_System_With_ADO.Net.Models
+{
+    public class ProductModel
+    {
+        DataAccess dataAccess;
+        public ProductModel()
+        {
+            dataAccess = new DataAccess();
+        }
+
+        public List<Product> GetProducts()
+        {
+            string sql = "SELECT * FROM Products";
+            SqlDataReader reader = dataAccess.GetData(sql);
+            List<Product> products = new List<Product>();
+            while(reader.Read())
+            {
+                Product product = new Product();
+                product.ProductId = (int)reader["ProductId"];
+                product.ProductName = reader["Productname"].ToString();
+                product.CategoryId = (int)reader["CategoryId"];
+                products.Add(product);
+            }
+            return products;
+        }
+    }
+}
