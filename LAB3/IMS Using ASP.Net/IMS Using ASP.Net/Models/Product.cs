6dbd62168f1cf481d516b024a22c45659c8941e2
NayeemHasan807 <moonnayeem87@gmail.com>
2021-02-24 18:59:47 +0600

    Product class

---
+++ LAB3/IMS Using ASP.Net/IMS Using ASP.Net/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_Management_System_With_ADO.Net.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
