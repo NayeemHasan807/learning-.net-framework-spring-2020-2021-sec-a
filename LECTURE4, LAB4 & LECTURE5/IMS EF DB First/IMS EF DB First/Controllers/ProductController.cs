7c3a3ad89502a0a3096b1b0a4154f731c575b29d
NayeemHasan807 <moonnayeem87@gmail.com>
2021-02-28 14:58:25 +0600

    Product CRUD using Scaffolding technique

---
+++ LECTURE4, LAB4 & LECTURE5/IMS EF DB First/IMS EF DB First/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using IMS_EF_DB_First.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS_EF_DB_First.Controllers
+{
+    public class ProductController : Controller
+    {
+        InventoryDbContext context = new InventoryDbContext();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(context.Products.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewData["categories"] = context.Categories.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ViewData["categories"] = context.Categories.ToList();
+            return View(context.Products.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Product product)
+        {
+            product.productid = id;
+            var productToEdit = context.Products.Find(id);
+            productToEdit.productname = product.productname;
+            productToEdit.price= product.price;
+            productToEdit.categoryid = product.categoryid;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View(context.Products.Find(id));
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View(context.Products.Find(id));
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            context.Products.Remove(context.Products.Find(id));
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
