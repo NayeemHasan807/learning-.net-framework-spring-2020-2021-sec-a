cb855d86a41dfb332d5b85fe0f981d13dfda4e52
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-09 22:06:50 +0600

    Checkout module

---
+++ Halloween Superstor Shopping Cart/ShoppingCartMVC/Ch24ShoppingCartMVC/Models/CheckoutModel.cs
@@ -0,0 +1,32 @@
+﻿using Ch24ShoppingCartMVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ch24ShoppingCartMVC.Models
+{
+    public class CheckoutModel
+    {
+        
+        public CheckoutViewModel GetCheckout()
+        {
+            CheckoutViewModel checkout = new CheckoutViewModel();
+            CartModel cart = new CartModel();
+            CartViewModel selectedProduct = cart.GetCart();
+            checkout.Checkout = selectedProduct.Cart;
+            foreach (var item in checkout.Checkout)
+            {
+                var pfori = item.Quantity * item.UnitPrice;
+                checkout.TotalPrice = checkout.TotalPrice + pfori;
+            }
+            checkout.TotalPriceWithTax = ((checkout.TotalPrice * 10) / 100) + checkout.TotalPrice;
+            return checkout;
+        }
+
+        public void AddCheckout(PaymentViewModel payment)
+        {
+            HttpContext.Current.Session["Payment"] = payment;
+        }
+    }
+}
---
+++ Halloween Superstor Shopping Cart/ShoppingCartMVC/Ch24ShoppingCartMVC/Models/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ch24ShoppingCartMVC.Models.ViewModels
+{
+    public class PaymentViewModel
+    {
+        [Required]
+        public string PaymentMethod { get; set; }
+        [Required,MinLength(8)]
+        public string Address { get; set; }
+        [Required,MinLength(4)]
+        public string City { get; set; }
+        [Required,MinLength(3)] 
+        public string State { get; set; }
+        [Required(ErrorMessage = "Zip code is required"),MinLength(3, ErrorMessage = "Zip code must need to be 3 digit number"),MaxLength(3, ErrorMessage = "Zip code must need to be 3 digit number"), Range(001,999, ErrorMessage = "Zip code must need to be 3 digit number")]
+        public String ZipCode { get; set; }
+
+    }
+}
