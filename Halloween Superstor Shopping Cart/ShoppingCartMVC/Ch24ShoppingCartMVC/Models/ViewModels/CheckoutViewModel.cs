6bd176ae8459118cf08748337057d7a9458602a3
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-09 20:40:48 +0600

    Halloween Superstor Shopping Cart necessary files with Home & Contact Page.

---
+++ Halloween Superstor Shopping Cart/ShoppingCartMVC/Ch24ShoppingCartMVC/Models/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ch24ShoppingCartMVC.Models.ViewModels
+{
+    public class CheckoutViewModel
+    {
+        public List<ProductViewModel> Checkout { get; set; }
+
+        public decimal TotalPrice { get; set; }
+        public decimal TotalPriceWithTax { get; set; }
+
+    }
+}
